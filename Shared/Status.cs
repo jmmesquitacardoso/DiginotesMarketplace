540791f61cc5241efec5338376fa2b1cddaa6c10
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2015-04-03 18:57:46 +0100

    forgot to add a class

---
+++ Shared/Status.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Shared
+{
+    class Status
+    {
+        enum UserAccess { Valid, Invalid, StubError };
+
+        private static Status instance;
+        
+        public static Status Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Status();
+                }
+                return instance;
+            }
+        }
+    }
+}
