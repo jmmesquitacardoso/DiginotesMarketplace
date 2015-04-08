f56a7250838d2fa260a06f7762f28f07a7b774c5
diogojapinto <diogojapinto@gmail.com>
2015-04-08 14:02:04 +0100

    database is persistent now

--- Remotes/Database.cs
+++ Remotes/Database.cs
@@ -20,24 +20,14 @@ public class Database
 
 	public Database ()
 	{
-		/*IFormatter formatter = new BinaryFormatter();
-            if (File.Exists("diginotes_database.bin"))
-            {
-                Stream stream = new FileStream("diginotes_database.bin",
-                    FileMode.Open,
-                    FileAccess.Read,
-                    FileShare.Read);
-                // Database obj = (Database) formatter.Deserialize(fromStream);
-                stream.Close();
-            }*/
 	}
 
 	private void SaveDatabase ()
 	{
 		IFormatter formatter = new BinaryFormatter ();
 		Stream stream = new FileStream ("diginotes_database.bin", 
-			                 FileMode.Create, 
-			                 FileAccess.Write, FileShare.None);
+			                FileMode.Create, 
+			                FileAccess.Write, FileShare.None);
 		formatter.Serialize (stream, this);
 		stream.Close ();
 	}
@@ -45,7 +35,20 @@ public class Database
 	public static Database Instance {
 		get {
 			if (instance == null) {
-				instance = new Database ();
+
+				// Load the database if it exists
+				IFormatter formatter = new BinaryFormatter ();
+				if (File.Exists ("diginotes_database.bin")) {
+					Stream stream = new FileStream ("diginotes_database.bin",
+						                FileMode.Open,
+						                FileAccess.Read,
+						                FileShare.Read);
+					instance = (Database) formatter.Deserialize(fromStream);
+					stream.Close ();
+				} else {
+					instance = new Database ();
+				}
+
 			}
 			return instance;
 		}
@@ -54,6 +57,7 @@ public class Database
 	public void AddUser (string key, User value)
 	{
 		registeredUsers.Add (key, value);
+		SaveDatabase()
 	}
 
 	public User getUserByUsername (string username)
